b54c312a8e1934527bde1efade08dce1ffb8c5f6
wojcikm11 <61560837+wojcikm11@users.noreply.github.com>
2021-11-21 19:54:31 +0100

    Project init

---
+++ src/test/java/pl/edu/pw/AsanaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsanaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
